03cfdadb8fa846b134af16556fcb457d9e233209
anthony <anthonytran.dev@gmail.com>
2021-07-11 15:01:23 +0100

    refactor extract out event handlers

--- index.js
+++ index.js
@@ -3,18 +3,22 @@
 var defaultText = 'Hello World!';
 var InputField;
 var Form;
-var text;
+
+function handleChange(event) {
+	text = event.target.value;
+}
+
+function handleSubmit(event) {
+	event.preventDefault();
+}
+
 window.onload = function () {
 	InputField = document.getElementById('echo-input');
 	Form = document.getElementById('form');
 
 	InputField.setAttribute('value', defaultText);
 
-	InputField.addEventListener('change', function (event) {
-		text = event.target.value;
-	});
+	InputField.addEventListener('change', handleChange);
 
-	Form.addEventListener('submit', function (event) {
-		event.preventDefault();
-	});
+	Form.addEventListener('submit', handleSubmit);
 };
