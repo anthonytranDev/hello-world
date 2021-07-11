bc9eb746cdc1e3f371da3357b4c922b04cc0d1c8
anthony <anthonytran.dev@gmail.com>
2021-07-11 14:58:25 +0100

    add submission to form init

--- index.js
+++ index.js
@@ -2,12 +2,19 @@
 
 var defaultText = 'Hello World!';
 var InputField;
+var Form;
 var text;
 window.onload = function () {
 	InputField = document.getElementById('echo-input');
+	Form = document.getElementById('form');
+
 	InputField.setAttribute('value', defaultText);
 
 	InputField.addEventListener('change', function (event) {
 		text = event.target.value;
+	});
+
+	Form.addEventListener('submit', function (event) {
+		event.preventDefault();
 	});
 };
