cd6db2aac6573c4511a297405dd15c45cf92a9c8
anthony <anthonytran.dev@gmail.com>
2021-07-11 14:42:00 +0100

    add input field with default value

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var defaultText = 'Hello World!';
+var InputField;
+window.onload = function () {
+	InputField = document.getElementById('echo-input');
+	InputField.setAttribute('value', defaultText);
+
+	});
+};
