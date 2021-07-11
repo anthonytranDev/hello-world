d10a0feac0c6f068344b305df3c33d2f071cc8b6
anthony <anthonytran.dev@gmail.com>
2021-07-11 14:42:43 +0100

    add change event listener to input field

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 
 var defaultText = 'Hello World!';
 var InputField;
+var text;
 window.onload = function () {
 	InputField = document.getElementById('echo-input');
 	InputField.setAttribute('value', defaultText);
 
+	InputField.addEventListener('change', function (event) {
+		text = event.target.value;
 	});
 };
